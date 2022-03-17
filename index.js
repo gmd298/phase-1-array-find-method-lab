2834aa7cb01b00b044263506f1ab91f74e231674
Griffin <gmd298@gmail.com>
2022-03-16 23:14:10 -0400

    commit tree 971da052ba54d20777a33cbb3947da1275c44198

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(year => year.result === "W")
+    if (result === undefined) {
+        return undefined
+    }
+    return result.year
+}
+//     if (superbowlWin === true) {
+        
+//     }
+// }
+
+
+
+// function superbowlWin(record) {
+//     const result = record.find(year => year.result === "W") 
+//     return (result.year) 
+// }
+
+// function superbowlWin(record) {
+//     return record.result === "W";
+// }
+
+// return (record.find(superbowlWin).year);
